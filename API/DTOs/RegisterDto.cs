b87b92f591aac9d802847dbc5463f1398cfe50d2
Patrick Baxter <baxterp159@gmail.com>
2022-05-26 01:00:03 -0400

    Section 7 add backend error handling ExceptionMiddleware.cs

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,7 +11,9 @@ namespace API.DTOs
         // The properties we're getting in the body of the request from the front end. 
         [Required]
         public string Username { get; set; }
+
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
