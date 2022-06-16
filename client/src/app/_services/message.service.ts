cdfa7a5e34db54853e2fbd9b6b0d9c9908d71c89
Patrick Baxter <baxterp159@gmail.com>
2022-06-15 23:41:32 -0400

    Section 15 styled member messages

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -17,4 +17,9 @@ export class MessageService {
     params = params.append('Container', container);
     return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http)
   }
+
+  // TODO make paginated. 
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+  }
 }
