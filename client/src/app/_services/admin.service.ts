46524668f4b359dc72b798a9756477afc6c49ac7
Patrick Baxter <baxterp159@gmail.com>
2022-06-17 00:39:26 -0400

    Fixed 404 error

--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
