68bcd476d800966a19ba3c695d393ea64b49ad7c
Patrick Baxter <baxterp159@gmail.com>
2022-06-16 23:45:25 -0400

    Section 16 added user management component

---
+++ client/src/app/_services/admin.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment.prod';
+import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getUsersWithRoles() {
+    return this.http.get<Partial<User[]>>(this.baseUrl + 'admin/users-with-roles');
+  }
+}
