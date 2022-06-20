121c2ab76975a485e5fb70edd4cb371426241a50
Patrick Baxter <baxterp159@gmail.com>
2022-06-19 22:44:08 -0400

    End of Section 18 updated styles for messages

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,13 +1,14 @@
 import { NgForm } from '@angular/forms';
 import { MessageService } from './../../_services/message.service';
 
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Message } from 'src/app/_models/message';
 
 @Component({
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
-  styleUrls: ['./member-messages.component.css']
+  styleUrls: ['./member-messages.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm: NgForm;
